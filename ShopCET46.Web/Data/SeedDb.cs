8d88753d5f2a278ffdef10f58c6ad45c13537667
Ricardo Almeida <Ricardo.Neiva.Almeida@formandos.cinel.pt>
2020-06-07 23:03:11 +0100

    SeedDb

---
+++ ShopCET46.Web/Data/SeedDb.cs
@@ -0,0 +1,44 @@
+﻿using ShopCET46.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCET46.Web.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+        private Random _random;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+            _random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+
+            if (!_context.Products.Any())
+            {
+                this.AddProduct("Boné do Glorioso");
+                this.AddProduct("Cuecas do Glorioso");
+                this.AddProduct("Biquini do Glorioso");
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+            _context.Products.Add(new Product
+            {
+                Name = name,
+                Price = _random.Next(500),
+                IsAvailable = true,
+                Stock = _random.Next(1000)
+            });
+        }
+    }
+}
