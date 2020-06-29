58bf5f758ca59f67d75ce9e7d196f142ccf9d12a
Ricardo Almeida <Ricardo.Neiva.Almeida@formandos.cinel.pt>
2020-06-29 18:33:29 +0100

    Add and Edit images

---
+++ ShopCET46.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopCET46.Web.Data;
+
+namespace ShopCET46.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_productRepository.GetAll());
+        }
+
+        /*if(ModelState.IsValid)
+         {
+            var path = string.Empty;
+            if(model.ImageFile != null && model.ImageFile.Length > 0)
+            {
+                path = Path.Combine(
+                    Directory.GetCurrentDirectory(),
+                    "wwwroot\\images\\Products",
+                    model.ImageFile.FileName);
+        
+                using(var stream = new FileStream(path, FileMode.Create))
+                {
+                    await model.ImageFile.CopyToAsync(stream);
+                }
+                path = $"~/images/Products/{model.ImageFile.FileName}";
+            }
+            var product = this.ToProduct(model, path);
+        
+         private Product ToProduct(ProductViewModel view, string path)
+        {
+            return new Product
+            {
+                Id = view.Id,
+                ImageUrl = path,
+                IsAvailable = view.IsAvailable,
+                LastPurchase = view.LastPurchase,
+                LastSale = view.LastSale,
+                Name = view.Name,
+                Price = view.Price,
+                Stock = view.Stock,
+                User = view.User};}
+
+        private ProductViewModel ToProductViewModel(Product product)
+        {
+            return new ProductViewModel
+        {
+            Id = view.Id,
+                ImageUrl = path,
+                IsAvailable = view.IsAvailable,
+                LastPurchase = view.LastPurchase,
+                LastSale = view.LastSale,
+                Name = view.Name,
+                Price = view.Price,
+                Stock = view.Stock,
+                User = view.User};}
+        */
+    }
+}
---
+++ ShopCET46.Web/Models/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using ShopCET46.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCET46.Web.Models
+{
+    public class ProductViewModel : Product
+    {
+        [Display(Name="Image")]
+        public IFormFile Image { get; set; }
+    }
+}
